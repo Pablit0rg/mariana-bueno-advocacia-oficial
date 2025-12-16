5feda2f4f3b05bc0d24ddba4e65babff61595757
Pablit0rg <pablorosagomes@gmail.com>
2025-12-16 09:10:46 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com', // Autoriza o Pexels
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com', // Autoriza o Unsplash (se usarmos)
+      },
+    ],
+  },
 };
 
 export default nextConfig;
