875ca2233ec0ab8b1ed6e37db2ee50d370354369
Pablit0rg <pablorosagomes@gmail.com>
2025-12-16 10:43:41 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,35 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Otimização de Imagens (já tínhamos)
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.pexels.com', // Autoriza o Pexels
+        hostname: 'images.pexels.com',
       },
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com', // Autoriza o Unsplash (se usarmos)
+        hostname: 'images.unsplash.com',
       },
     ],
+    minimumCacheTTL: 60 * 60 * 24 * 30, // Cache de imagens por 30 dias
+  },
+
+  // Cabeçalhos de Cache (A "Memória" do Navegador)
+  async headers() {
+    return [
+      {
+        source: '/:all*(svg|jpg|png)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable', // Grava por 1 ano!
+          },
+        ],
+      },
+    ];
   },
 };
 
