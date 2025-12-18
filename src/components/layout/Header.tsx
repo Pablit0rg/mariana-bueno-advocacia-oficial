75327dc16a35093f087e94e6a060806ace0d49ef
Pablit0rg <pablorosagomes@gmail.com>
2025-12-18 11:04:52 -0300

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -59,8 +59,8 @@ export function Header() {
             <Image 
               src="/logo-header.png-removebg-preview.png"
               alt="Mariana Bueno Advocacia" 
-              width={68}
-              height={68}
+              width={70}
+              height={70}
               className="object-contain hover:scale-105 transition-transform duration-300 -mt-2" 
               priority
             />
