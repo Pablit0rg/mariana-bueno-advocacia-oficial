44679088d8e28157ebcb95ff7533c268e3335e85
Pablit0rg <pablorosagomes@gmail.com>
2025-12-23 23:07:48 -0300

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -13,7 +13,7 @@ export function Header() {
   useEffect(() => {
     const handleScroll = () => {
       // Ativa a transição logo após sair do topo (100px)
-      setScrolled(window.scrollY > 10);
+      setScrolled(window.scrollY > 05);
     };
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
